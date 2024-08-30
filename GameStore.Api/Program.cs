a4f6baa7e4b976984c1417ff759ee8e11c10b1b2
ErickAC12 <112605408+ErickAC12@users.noreply.github.com>
2024-08-29 20:26:01 -0600

    games/id api

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -29,8 +29,10 @@ List<GameDto> games = [
     )
 ];
 
+// GET /games
 app.MapGet("games", () => games);
 
-app.MapGet("/", () => "Hello World!");
+// GET /games/1
+app.MapGet("games/{id}", (int id) => games.Find(game => game.Id == id));
 
 app.Run();
--- GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35f965c8eb2b7203b4aa63de51efa218aad70158")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4fa5c988a21b2fb634e853abac3bbff78cf1b08")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
