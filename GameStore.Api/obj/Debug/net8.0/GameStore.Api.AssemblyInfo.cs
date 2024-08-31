b44fbf21e9209fd7829adb1f3e9d7c809e96e409
ErickAC12 <112605408+ErickAC12@users.noreply.github.com>
2024-08-30 19:20:46 -0600

    basic Db structure

--- GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cb3cdeb566ba5a34e7cafa28b26a0613b0bacff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f07c23f393d255de38da64e82ddce585fb01889")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
